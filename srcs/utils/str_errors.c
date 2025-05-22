16249f38d58637594d494d43865df096a16fa20e
MIRAUD Quentin <quentin.miraud@icloud.com>
2025-05-22 15:11:05 +0200

    refacto pour voir?

--- srcs/utils/str_errors.c
+++ srcs/utils/str_errors.c
@@ -6,7 +6,7 @@
 /*   By: qumiraud <qumiraud@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/22 12:09:06 by qumiraud          #+#    #+#             */
-/*   Updated: 2025/05/22 12:18:51 by qumiraud         ###   ########.fr       */
+/*   Updated: 2025/05/22 14:49:55 by qumiraud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 void	str_error(char *str, char *cmd_args, char *msg)
 {
 	write(2, str, ft_strlen(str));
-	if (cmd_args[0] != 0)
+	if (cmd_args && cmd_args[0] != '\0')
 	{
 		write(2, cmd_args, ft_strlen(cmd_args));
 		write(2, " : ", 3);
